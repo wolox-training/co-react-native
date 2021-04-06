3ac23e35cebd726ebe68716eb34a49315c2d5dee
Camilo <camilo.ortiz@wolox.co>
2021-04-06 11:42:03 -0500

    revert to mock

--- wbooks/src/services/BookService.ts
+++ wbooks/src/services/BookService.ts
@@ -1,3 +1,8 @@
-import api from '@config/api';
+import { BOOKS_MOCK } from '@constants/mockBooks';
+import { BooksResponse } from '@interfaces/book';
 
-export const getBookList = () => api.get('/books');
+export function getBookList() {
+  return new Promise<BooksResponse>(resolve => {
+    setTimeout(() => resolve({ ok: true, data: BOOKS_MOCK }), 1000);
+  });
+}
