61b00a73dd6868398b99849a31a38fdd115c38f8
Camilo <camilo.ortiz@wolox.co>
2021-04-09 12:36:13 -0500

    Animations implementation in ItemBook component

--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -31,8 +31,10 @@ function Library({ navigation }: Props) {
 
   const handleTouch = () => navigation.navigate(Screens.BOOK_DETAIL);
   const renderBooks: ListRenderItem<Book> = ({ item }) => {
-    const { imageUrl, title, author } = item;
-    return <ItemBook image={imageUrl} onPress={handleTouch} title={trimLineBreak(title)} author={author} />;
+    const { id, imageUrl, title, author } = item;
+    return (
+      <ItemBook id={id} image={imageUrl} onPress={handleTouch} title={trimLineBreak(title)} author={author} />
+    );
   };
   const keyExtractor = ({ id }: Book) => String(id);
 
